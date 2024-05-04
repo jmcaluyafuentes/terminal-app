08d574c344bcebf35dd8248851ce6e30c1d886e8
John Fuentes <jmcaluyafuentes@gmail.com>
2024-05-04 11:16:01 +1000

    Add trimester calculation

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 from datetime import datetime
 
 def calculate_gestational_age(last_period_date):
+
     # Get the current date
     current_date = datetime.now()
-    print(current_date)
 
     # Calculate elapsed time from last period to current date in days
     elapsed_time = (current_date - last_period_date).days
-    print(elapsed_time)
 
-    # Calculate gestational age in weeks (round down)
+    # Calculate gestational age in weeks and round down the result
     gestational_age_in_weeks = elapsed_time // 7
-    print(gestational_age_in_weeks)
+    return gestational_age_in_weeks
 
-last_period_month = int(input('Enter the month of last period: '))
-last_period_day = int(input('Enter the day of last period: '))
-last_period_year = int(input('Enter the year of last period: '))
+def calculate_trimester(gestational_age):
+    if gestational_age <= 12:
+        return 'first trimester'
+    elif 13 <= gestational_age <= 26:
+        return 'second trimester'
+    else:
+        return 'third trimester'
 
-last_period_date = datetime(last_period_year, last_period_month, last_period_day)
+# Main function
+def main():
 
-calculate_gestational_age(last_period_date)
+    # Prompt the use to enter the month, day and year of her last period
+    last_period_month = int(input('Enter the month of last period: '))
+    last_period_day = int(input('Enter the day of last period: '))
+    last_period_year = int(input('Enter the year of last period: '))
+    last_period_date = datetime(last_period_year, last_period_month, last_period_day)
+
+    # Calculate gestational age in weeks
+    gestational_age = calculate_gestational_age(last_period_date)
+
+    # Calculate trimester based on gestational age
+    trimester = calculate_trimester(gestational_age)
+
+    # Display the output
+    print(f'You are {gestational_age} weeks pregnant and in {trimester}.')
+
+if __name__ == "__main__":
+    main()
