fcc4e4f9a6563a8bfd089436aee8c04e93b9a3b4
John Fuentes <jmcaluyafuentes@gmail.com>
2024-05-10 02:02:00 +1000

    Refactor code in activities_safety module

--- activities_safety.py
+++ activities_safety.py
@@ -3,7 +3,6 @@ This module will give information about safe activities for pregnant women.
 """
 
 import csv
-from textwrap import dedent
 from print_guide import guide, guide_user_response
 
 def check_activities_safety() -> None:
@@ -17,26 +16,34 @@ def check_activities_safety() -> None:
         # Display the guide for instructions and for quitting the app
         guide() # From print_guide module
 
-        print(dedent('''
-        The following are topics about safe activities for pregnant women.\n
-        1. Benefits of exercising
-        2. Exercises for pregnant women
-        3. Activities that can be done outdoors
-        '''))
+        print('The following are topics about safe activities for pregnant women.\n')
 
-        user_choice = input('Enter your choice (1, 2 or 3): ')
+        # Topics presented in a list
+        topics = [
+            'Benefits of exercising',
+            'Exercises for pregnant women',
+            'Activities that can be done outdoors'
+        ]
+
+        # Display the topics with indices from the list
+        for index, topic in enumerate(topics, 1):
+            print(f'{index}. {topic}')
+
+        # Get the user choice
+        user_choice = input('\nEnter your choice (1, 2 or 3): ')
 
         # Check if user want to view the instructions or exits the app
         guide_user_response(user_choice)
 
         activity_loop = True
 
+        # Check the user input and identify the topic
         if user_choice == '1':
-            topic = 'Benefits of exercising'
+            topic = topics[int(user_choice) - 1]
         elif user_choice == '2':
-            topic = 'Exercises for pregnant women'
+            topic = topics[int(user_choice) - 1]
         elif user_choice == '3':
-            topic = 'Activities that can be done outdoors'
+            topic = topics[int(user_choice) - 1]
         else:
             print('Error: Invalid choice. Please enter 1 for Benefits, 2 for Exercises or 3 for Outdoor\n')
             activity_loop = False
@@ -44,18 +51,25 @@ def check_activities_safety() -> None:
         print(f'\n{topic}:')
 
         while activity_loop:
+
             # Open the file activities_safety.csv in read mode in context manager
             with open('activities_safety.csv', encoding="utf-8") as f:
+
                 # Create a dict object named reader
                 reader = csv.DictReader(f)
+
                 # Iterate over every row in the file
                 for row in reader:
+
                     # Check if the value in 'Topic' column matches the user's entered topic
                     if row['Topic'].lower() == topic.lower():
-                        # If match found, it extract the information from the current row
+
+                        # If match found, extract the information from the current row
                         infos = [row['Info 1'], row['Info 2'], row['Info 3'], row['Info 4'], row['Info 5']]
-                        # Display activities safety information with indices
+
+                        # Extract the index and value pair in infos list
                         for index, activity_info in enumerate(infos, 1):
+
                             # Display only if travel info is not an empty string
                             if activity_info:
                                 print(f'{index}. {activity_info}')
@@ -91,5 +105,5 @@ def get_user_next_action() -> bool:
             guide() # From print_guide module
 
             # Inform the user that she entered invalid choice
-            print('Invalid choice. Please enter 1 or 2.')
+            print('ERROR: Invalid choice. Please enter 1 or 2.')
 
